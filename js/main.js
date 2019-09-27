90dbe593ce0b9f0e72e51a6c46e387bc4fe0aa29
Дима <dmitriy.golovin.2001.03@mail.ru>
2019-09-27 19:42:11 +0300

    Исправил ошибку № 3

---
+++ js/main.js
@@ -0,0 +1,72 @@
+'use strict'
+var NUMBER_TAGS = 8;
+var tags = [];
+var MIN_ADDRESS = 130;
+var MAX_ADDRESS = 630;
+var MIN_COORDINATE = 130;
+var MAX_COORDINATE = 630;
+var MAX_ADDRESS = 630;
+var MIN_ROOMS = 2;
+var MAX_ROOMS = 7;
+var clonedLabel;
+var map__pins = document.querySelector(".map__pins");
+var map__pin = document.querySelector(".map__pin");
+var elementMap = document.querySelector(".map");
+var description = ['Есть газовая печка стиральная машина синии стены']
+var PHOTO_ADDRESS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg', 'http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg', 'http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg'];
+var features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner", "wifi parking", "wifi washer"];
+var fragment = document.createDocumentFragment();
+
+function getRandomInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+elementMap.classList.remove("map--faded");
+for (var i = 0; i < NUMBER_TAGS; i++) {
+  var randomLocationX = getRandomInRange(MIN_COORDINATE, MAX_COORDINATE);
+  var randomLocationY = getRandomInRange(MIN_COORDINATE, MAX_COORDINATE);
+  var j = i--;
+    while (i > 0 && j > 0) {
+      if (tags[j].location.X === randomLocationX) {
+        var randomLocationX = getRandomInRange(MIN_COORDINATE, MAX_COORDINATE);
+        j = i - 1;
+
+      }
+      if (tags[j].location.Y === randomLocationY) {
+        var randomLocationY = getRandomInRange(MIN_COORDINATE, MAX_COORDINATE);
+        j = i - 1;
+
+      }
+      if (tags[j].location.X !== randomLocationX && tags[j].location.Y !== randomLocationY) {
+          j--;
+      }
+    }
+  tags[i] = {
+    author: {
+      avatar: 'img/avatars/user0' + getRandomInRange(1, 8) + '.png'
+    },
+
+    offer: {
+      title: 'Заголовок объявления',
+      address: getRandomInRange(MIN_ADDRESS, MAX_ADDRESS) + ',' + getRandomInRange(MIN_ADDRESS, MAX_ADDRESS),
+      price: 1000,
+      type: 'palace',
+      rooms: getRandomInRange(MIN_ROOMS, MAX_ROOMS),
+      guests: getRandomInRange(1, 5),
+      checkin: '12:00',
+      checkout: '12:00',
+      features: features[i],
+      description: 'Есть газовая печка стиральная машина синии стены',
+      photos: PHOTO_ADDRESS[i],
+    },
+
+    location: {
+      x: randomLocationX,
+      y: randomLocationY
+    }
+  }
+  clonedLabel = map__pin.cloneNode(true);
+  clonedLabel.setAttribute('style', 'left:' + tags[i].location.x + '; top:' + tags[i].location.y + ';');
+  clonedLabel.querySelector('img').src = tags[i].photos;
+  elementMap.insertBefore(clonedLabel, map__pins);
+
+}
