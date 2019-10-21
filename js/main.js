ceb2e5bee721e991ed270e55d23505b92661e43c
Дима <dmitriy.golovin.2001.03@mail.ru>
2019-10-21 16:48:21 +0300

    Исправил ошибку.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ var MIN_ADDRESS = 130;
 var MAX_ADDRESS = 630;
 var MIN_COORDINATE = 130;
 var MAX_COORDINATE = 630;
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner', 'wifi parking', 'wifi washer'];
+var FEATURES = ['wifi dishwasher', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner', 'wifi parking', 'wifi washer'];
+var convenienceName=['wifi','dishwasher','parking','washer','elevator','conditioner']
+var convenienceIcon=[];
 var MIN_ROOMS = 2;
 var MAX_ROOMS = 7;
 var tags = [];
@@ -19,7 +21,7 @@ var fieldsets = document.querySelectorAll('fieldset');
 var fragment = document.createDocumentFragment();
 var popup = document.querySelector('#card').content.querySelector('.popup');
 var clonePopup;
-
+var wordBeginnings=0;
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -74,7 +76,7 @@ for (var i = 0; i < TAGS_NUMBER; i++) {
   };
   makeMark({
     author: {
-      avatar: 'img/avatars/user0' + getRandomInRange(1, 8) + '.png'
+      avatar: 'img/avatars/user0' + (i + 1) + '.png'
     },
 
     offer: {
@@ -99,9 +101,67 @@ for (var i = 0; i < TAGS_NUMBER; i++) {
 }
 
 activatePage(true);
-
+var popupTitle = popup.querySelector('.popup__title');
+var popupTextAddress=popup.querySelector('.popup__text--address'),
+    popupTextPrice=popup.querySelector('.popup__text--price'),
+    popupType=popup.querySelector('.popup__type'),
+    popupTextCapacity=popup.querySelector('.popup__text--capacity'),
+    popupTextTime=popup.querySelector('.popup__text--time'),
+    popupFeatures=popup.querySelector('.popup__features'),
+    popupFeature=popupFeatures.querySelectorAll('.popup__feature'),
+    convenienceIcon=[
+      popupFeatures.querySelector('.popup__feature--wifi'),
+      popupFeatures.querySelector('.popup__feature--dishwasher'),
+      popupFeatures.querySelector('.popup__feature--parking'),
+      popupFeatures.querySelector('.popup__feature--washer'),
+      popupFeatures.querySelector('.popup__feature--elevator'),
+      popupFeatures.querySelector('.popup__feature--conditioner')
+      ];
+    for(i=0; i<popupFeature.length; i++)
+      {
+    popupFeature[i].style.display='none';
+      }
 mapPinMain.addEventListener('mousedown', function () {
   activatePage(false);
+
+  mapPins.onmousedown = function (evt) {
+  if (evt.target.tagName === 'BUTTON' || evt.target.tagName === 'IMG') {
+    console.log(evt);
+    popupTitle.innerHTML = tags[0].offer.title;
+    popupTextAddress.textContent=tags[0].offer.address;
+    popupTextPrice.textContent=tags[0].offer.price+'₽/ночь';
+    switch(tags[0].offer.type)
+      {
+        case 'flat':
+          popupType.textContent='Квартира';
+          break;
+        case 'bungalo':
+          popupType.textContent='Бунгало';
+          break;
+        case 'house':
+          popupType.textContent='Дом';
+        case 'palace':
+          popupType.textContent='Дворец';
+          break;
+      }
+    popupTextCapacity.textContent=tags[0].offer.rooms+' комнаты для '+tags[0].offer.guests+' гостей';
+    popupTextTime.textContent='Заезд после '+tags[0].offer.checkin+' выезд до '+tags[0].offer.checkout;
+    top:
+    for(i=0; i<convenienceName.length; i++)
+      {
+        if(tags[0].offer.features.substring(wordBeginnings,wordBeginnings+convenienceName[i].length)===convenienceName[i])
+        {
+         convenienceIcon[i].style.display='block';
+         wordBeginnings+=convenienceName[i].length+1;
+            while(tags[0].offer.features.sharAt(wordBeginnings)===' ')
+            {
+              wordBeginnings++;
+            }
+            break top;
+        }
+      }
+  }
+}
 });
 
 function onMapPinMainPress(evt) {
@@ -110,8 +170,3 @@ function onMapPinMainPress(evt) {
   }
 }
 mapPinMain.addEventListener('keydown', onMapPinMainPress);
-mapPins.onmousedown = function (evt) {
-  if (evt.target.tagName === 'BUTTON' || evt.target.tagName === 'IMG') {
-    console.log(evt);
-  }
-}
